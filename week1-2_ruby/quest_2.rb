bafd415e39ba1c8f31710b598f859ff2003062b7
777yuka <y.matsunaga810@gmail.com>
2023-04-11 12:44:12 +0900

    日報のフォルダを新規作成

--- week1-2_ruby/quest_2.rb
+++ week1-2_ruby/quest_2.rb
@@ -1,8 +1,6 @@
 #コードを手元で実行する
 
-puts "好きな数値を入力してください:"
+print "好きな数値を入力してください:"
 integer = gets.to_i
-puts "好きな数値を入力してください:#{integer}"
 double_integer = integer * 2
 puts "2倍の数値です:#{double_integer}"
-
